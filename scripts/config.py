cd8418f2e9f33d5e323823125522d6bd10597861
YY@alpha <ys7yoo@gmail.com>
2018-02-28 14:01:27 +0900

    data folder name: /tmp/data

--- scripts/config.py
+++ scripts/config.py
@@ -5,9 +5,9 @@ ROOT_DIR = os.path.expanduser('~/src/deeppose')
 OUTPUT_DIR = os.path.join(ROOT_DIR, 'out')
 
 # put data in /tmp
-LSP_DATASET_ROOT = os.path.expanduser('/tmp/lsp')
-LSP_EXT_DATASET_ROOT = os.path.expanduser('/tmp/lsp_ext')
-MPII_DATASET_ROOT = os.path.expanduser('/tmp/mpii')
+LSP_DATASET_ROOT = os.path.expanduser('/tmp/data/lsp')
+LSP_EXT_DATASET_ROOT = os.path.expanduser('/tmp/data/lsp_ext')
+MPII_DATASET_ROOT = os.path.expanduser('/tmp/data/mpii')
 
 #default location (old)
 #LSP_DATASET_ROOT = os.path.expanduser('~/data/lsp')
