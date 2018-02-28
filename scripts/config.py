637c97a9d142ea9c8fbcebcd504ff10d0f1d76b5
YY@alpha <ys7yoo@gmail.com>
2018-02-28 14:22:39 +0900

    moved data folder from /tmp/data to /var/data

--- scripts/config.py
+++ scripts/config.py
@@ -5,9 +5,9 @@ ROOT_DIR = os.path.expanduser('~/src/deeppose')
 OUTPUT_DIR = os.path.join(ROOT_DIR, 'out')
 
 # put data in /tmp
-LSP_DATASET_ROOT = os.path.expanduser('/tmp/data/lsp')
-LSP_EXT_DATASET_ROOT = os.path.expanduser('/tmp/data/lsp_ext')
-MPII_DATASET_ROOT = os.path.expanduser('/tmp/data/mpii')
+LSP_DATASET_ROOT = os.path.expanduser('/var/data/lsp')
+LSP_EXT_DATASET_ROOT = os.path.expanduser('/var/data/lsp_ext')
+MPII_DATASET_ROOT = os.path.expanduser('/var/data/mpii')
 
 #default location (old)
 #LSP_DATASET_ROOT = os.path.expanduser('~/data/lsp')
