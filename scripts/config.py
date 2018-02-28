111a9baac862a510dff3afbd59b5f9cf8f249e26
YY@alpha <ys7yoo@gmail.com>
2018-02-28 13:57:52 +0900

    changed data folder

--- scripts/config.py
+++ scripts/config.py
@@ -3,9 +3,13 @@ import os
 # Full path to the project root
 ROOT_DIR = os.path.expanduser('~/src/deeppose')
 OUTPUT_DIR = os.path.join(ROOT_DIR, 'out')
-#LSP_DATASET_ROOT = os.path.join(ROOT_DIR, 'datasets/lsp')
-LSP_DATASET_ROOT = os.path.expanduser('~/data/lsp')
-#LSP_EXT_DATASET_ROOT = os.path.join(ROOT_DIR, 'datasets/lsp_ext')
-LSP_EXT_DATASET_ROOT = os.path.expanduser('~/data/lsp_ext')
-#MPII_DATASET_ROOT = os.path.join(ROOT_DIR, 'datasets/mpii')
-MPII_DATASET_ROOT = os.path.expanduser('~/data/mpii')
+
+# put data in /tmp
+LSP_DATASET_ROOT = os.path.expanduser('/tmp/lsp')
+LSP_EXT_DATASET_ROOT = os.path.expanduser('/tmp/lsp_ext')
+MPII_DATASET_ROOT = os.path.expanduser('/tmp/mpii')
+
+#default location (old)
+#LSP_DATASET_ROOT = os.path.expanduser('~/data/lsp')
+#LSP_EXT_DATASET_ROOT = os.path.expanduser('~/data/lsp_ext')
+#MPII_DATASET_ROOT = os.path.expanduser('~/data/mpii')
