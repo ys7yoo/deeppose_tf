00878f30d55d66c0617d4df0e7b3e2c8e637528a
YY <ys7yoo@gmail.com>
2018-07-22 18:49:09 +0900

    added prefix DBG to debug output

---
+++ train/train_debug.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+PROJ_ROOT=$(pwd)
+
+DATA_ROOT=/var/data
+
+# info for output
+HOSTNAME=$(hostname)
+DATE_TIME=$(date +%Y.%m.%d_%R)
+
+
+CUDA_VISIBLE_DEVICES=${1} \
+PYTHONPATH=${PROJ_ROOT}:$PYTHONPATH \
+python ${PROJ_ROOT}/train/train.py \
+--max_iter 500000 \
+--batch_size 256 \
+--snapshot_step 10000 \
+--test_step 1000 \
+--log_step 10 \
+--train_csv_fn ${DATA_ROOT}/MET3/activity1.csv \
+--val_csv_fn ${DATA_ROOT}/MET3/activity1.csv \
+--test_csv_fn ${DATA_ROOT}/MET3/activity1.csv \
+--img_path_prefix=${DATA_ROOT}/MET3 \
+--n_joints 8 \
+--seed 1701 \
+--im_size 227 \
+--min_dim 6 \
+--shift 0.1 \
+--bbox_extension_min 1.2 \
+--bbox_extension_max 2.0 \
+--coord_normalize \
+--fname_index 0 \
+--dataset_name met \
+--joint_index 13 \
+--joint_index_end 29 \
+--symmetric_joints "[[2, 3], [1, 4], [0, 5]]" \
+--conv_lr 0.0005 \
+--fc_lr 0.0005 \
+--fix_conv_iter 10000 \
+--optimizer adam \
+--o_dir ${PROJ_ROOT}/out/DBG_${DATE_TIME}_met_n10_ub_alexnet_imagenet_${HOSTNAME}_gpu${1} \
+--gcn \
+--fliplr \
+--workers 8 \
+--net_type Alexnet \
+-s ${PROJ_ROOT}/weights/bvlc_alexnet.tf \
+--reset_iter_counter \
+--should_downscale_images
